63852f79fbbb8ed358222463c9db4f5895b128df
His Majesty <nyankson25@gmail.com>
2022-12-05 15:46:38 +0000

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -15,4 +15,4 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
